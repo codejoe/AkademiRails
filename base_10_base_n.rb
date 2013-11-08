3a3e98b6e7127d29749519409414361aa016f68f
Domikado 2 <domikado2@Domikado-2.local>
2013-11-08 17:59:42 +0700

    change base n to recursion

--- base_10_base_n.rb
+++ base_10_base_n.rb
@@ -1,28 +1,29 @@
 # Base 10 to Base N
-require "minitest/autorun"
+# require "minitest/autorun"
 
 def converttobase(number, basis, result="")
   array = %w(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ! @ # $ % ^ & * ( ) _ + - = { } [ ] < > ? ~)
   #result = ""
-  if number == 0
-    0
-  else
+  if number > 0
     modulus = number % basis
     #       = 5 % 2
     result += array[modulus] 
     #       = "" + 1   
     number = number / basis
     #       = 5 / 2
-    converttobase(number,basis,result)    
+    converttobase(number,basis,result) 
+  else   
+    result.reverse
   end
-  result.reverse
 end
 
-class MethodTesting < MiniTest::Unit::TestCase
-	def test_1
-		assert_equal '101',converttobase(5,2) 
-	end
-end
+puts converttobase(9,4)
+
+# class MethodTesting < MiniTest::Unit::TestCase
+# 	def test_1
+# 		assert_equal '21',converttobase(9,4) 
+# 	end
+# end
 
 #puts converttobase(9,2)
 #puts converttobase(9,2) == '1001'
