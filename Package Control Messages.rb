50689fffaeb78cf4362283896c9c7a5ab72b4d99
naufal <naufal@CN-LIN.(none)>
2013-11-11 08:43:16 +0700

    added The Well Grounded Rubyst.rb

---
+++ Package Control Messages.rb
@@ -0,0 +1,11 @@
+# Chapter 1 => Bootstrapping your Ruby literacy
+irb
+ruby syntax 
+
+# Chapter 2 => Objects, methods, and local variables
+
+anatomy of method definition
+return of value method
+order of parameters and arguments
+sample method of signatures arguments
+local variables and variables assignment
